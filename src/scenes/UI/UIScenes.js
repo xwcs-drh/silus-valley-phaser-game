868fd3f14a6dd529c922c06a94be66e190f829be
chloe-farr <chloe.l.farr@gmail.com>
2024-07-10 17:33:07 -0700

    Create UIScenes.js
    List of UI and popup scenes  - imported into BootScene, not index.js

---
+++ src/scenes/UI/UIScenes.js
@@ -0,0 +1,22 @@
+import MainUIScene from './MainUIScene';
+import VocabUIScene from './VocabUIScene';
+import TraditionalActivityUIScene from './TraditionalActivityUIScene';
+
+import InventoryPopupScene from './InventoryPopupScene';
+import RecipeBookPopupScene from './RecipeBookPopupScene';
+import SettingsPopupScene from './SettingsPopupScene';
+import ManualPopupScene from './ManualPopupScene';
+import CreditsPopupScene from './CreditsPopupScene';
+
+// Export all UI scenes here
+export {
+    MainUIScene,
+    VocabUIScene,
+    TraditionalActivityUIScene,
+    InventoryPopupScene,
+    RecipeBookPopupScene,
+    SettingsPopupScene,
+    ManualPopupScene,
+    CreditsPopupScene,
+    // Add other UI scenes here as added
+};
