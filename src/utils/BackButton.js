537093473862650645541f9770730eff51322759
chloe-farr <chloe.l.farr@gmail.com>
2024-07-16 15:18:10 -0700

    Update BackButton.js

--- src/utils/BackButton.js
+++ src/utils/BackButton.js
@@ -1,44 +1,54 @@
-import Phaser from 'phaser';
-
 export default class BackButton extends Phaser.GameObjects.Container {
-  constructor(scene, canvasWidth, canvasHeight, text, sceneManager) {
-    const x = canvasWidth*0.15;
-    const y = canvasHeight*0.1;
-    super(scene, x, y);
-
-    const textStyle = {
-        fontFamily: 'Unbounded',
-        fontSize: '20px',
-        fill: '#fff',
-        strokeThickness:0.5,
-        resolution: window.devicePixelRatio
-    };
-    const width = canvasWidth*0.5;
-    const height = canvasHeight*0.3;
-
-    console.log("back button: x: ", x, " y: ", y);
-    // Load the background image
-    const buttonBackground = scene.add.image(x, y, 'blueButtonBackground').setOrigin(0.5);
-    buttonBackground.setDisplaySize(width, height);
+  constructor(scene, canvasWidth, canvasHeight, sceneManager) {
+    super(scene, canvasWidth, canvasHeight);
     
-    //update button width to correspond to image dimensions
-    const buttonWidth = buttonBackground.displayWidth;
-    const buttonHeight = buttonBackground.displayHeight;
+    // console.log('Initializing BackButton');
+    
+    // Store the scene manager for later use
+    this.sceneManager = sceneManager;
 
-    // Create the text object
-    const buttonText = scene.add.text(x, y, text, textStyle).setOrigin(0.5);
+    // Calculate button position and size based on canvas dimensions
+    const x = canvasWidth * 0.07;
+    const y = canvasHeight * 0.05;
+    const width = canvasWidth * 0.11;
+    const height = canvasHeight * 0.06;
 
-    // Add background and text to the container
+    // console.log(`Button position: (${x}, ${y}), size: (${width}, ${height})`);
+
+    // Add the button background image
+    const buttonBackground = scene.add.image(0, 0, 'backButtonImg').setOrigin(0.5);
+    buttonBackground.setDisplaySize(width, height);
+
+
+    // Add the button background to the container
     this.add(buttonBackground);
-    this.add(buttonText);
 
-    // Make the button interactive
-    this.setSize(buttonWidth, buttonHeight);
-    this.setInteractive(new Phaser.Geom.Rectangle(x, y, buttonWidth, buttonHeight), Phaser.Geom.Rectangle.Contains)
-      .on('pointerdown',  () => sceneManager.goBack())
-      .on('pointerover', () => buttonBackground.setTint(0xAAAAAA))
-      .on('pointerout', () => buttonBackground.clearTint());
+    // Set the size of the container to match the button background
+    this.setSize(buttonBackground.displayWidth, buttonBackground.displayHeight);
 
-    // Add the button object to the scene
+    // Set the position of the container
+    this.setPosition(x, y);
+
+    // Make the button interactive and define its interactive area
+    this.setInteractive(new Phaser.Geom.Rectangle(0, 0, buttonBackground.displayWidth, buttonBackground.displayHeight), Phaser.Geom.Rectangle.Contains)
+      .on('pointerdown', () => {
+        console.log('Back button pressed');
+        sceneManager.goBack();
+      })
+      .on('pointerover', () => {
+        // console.log('Pointer over back button');
+        buttonBackground.setTint(0xAAAAAA);
+      })
+      .on('pointerout', () => {
+        // console.log('Pointer out of back button');
+        buttonBackground.clearTint();
+      });
+
+    // Add the container to the scene
+    scene.add.existing(this);
+    // console.log('BackButton added to scene');
+
+    // Log the final position of the button
+    // console.log(`Final button position: (${this.x}, ${this.y})`);
   }
 }
